010850e80fb661f1ee56a41a7c4d60d0df046599
Adrian <adrianahmadi06@gmail.com>
2025-10-30 11:21:28 -0400

    add number converter

---
+++ NumConverter/NumConverter.java
@@ -0,0 +1,37 @@
+package NumConverter;
+
+// work in progress
+
+public class NumConverter {
+    static int binMax = 7;
+
+    public static String toString(String[] arr) {
+        String str = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            str += arr[i];
+        }
+
+        return str;
+    }
+
+    public static String binaryConvert(int num) {
+        String[] res = new String[binMax];
+
+        for (int i = 0; i < binMax; i++) {
+            if (num % 2 == 1) {
+                res[i] = "1";
+                num = Math.round(num / 2);
+            } else {
+                res[i] = "0";
+                num = Math.round(num / 2);
+            }
+        }
+
+        return toString(res);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binaryConvert(87));
+    }
+}
