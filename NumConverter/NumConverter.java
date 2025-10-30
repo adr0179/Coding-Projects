b388e69b66c725cf599313175d3fc49db44a9776
Adrian <adrianahmadi06@gmail.com>
2025-10-30 12:03:09 -0400

    update num converter to conver bigger numbers to binary

--- NumConverter/NumConverter.java
+++ NumConverter/NumConverter.java
@@ -1,29 +1,28 @@
 package NumConverter;
+import java.util.ArrayList;
 
 // work in progress
 
 public class NumConverter {
-    static int binMax = 8;
-
-    public static String toString(String[] arr) {
+    public static String toString(ArrayList<String> arr) {
         String str = "";
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            str += arr[i];
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            str += arr.get(i);
         }
 
         return str;
     }
 
     public static String binaryConvert(int num) {
-        String[] res = new String[binMax];
+        ArrayList<String> res = new ArrayList<>();
 
-        for (int i = 0; i < binMax; i++) {
+        while (num >= 1) {
             if (num % 2 == 1) {
-                res[i] = "1";
+                res.add("1");
                 num = Math.round(num / 2);
             } else {
-                res[i] = "0";
+                res.add("0");
                 num = Math.round(num / 2);
             }
         }
@@ -36,5 +35,6 @@ public class NumConverter {
         System.out.println(binaryConvert(20));
         System.out.println(binaryConvert(255));
         System.out.println(binaryConvert(0));
+        System.out.println(binaryConvert(4532));
     }
 }
