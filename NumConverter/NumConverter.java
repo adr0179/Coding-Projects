bf14fa0c9f80a0b43638b5f4fb5d3d481d110e3c
Adrian <adrianahmadi06@gmail.com>
2025-10-31 10:03:11 -0400

    add method to convert decimal to hex

--- NumConverter/NumConverter.java
+++ NumConverter/NumConverter.java
@@ -34,6 +34,41 @@ public class NumConverter {
         return toString(res);
     }
 
+    public static String toHex(int num) {
+        ArrayList<String> res = new ArrayList<>();
+
+        if (num <= 9) {
+            return Integer.toString(num);
+        } 
+
+        while(num >= 1) {
+            if (num % 16 == 10) {
+                res.add("A");
+                num = Math.round(num / 16);
+            } else if (num % 16 == 11) {
+                res.add("B");
+                num = Math.round(num / 16);
+            } else if (num % 16 == 12) {
+                res.add("C");
+                num = Math.round(num / 16);
+            } else if (num % 16 == 13) {
+                res.add("D");
+                num = Math.round(num / 16);
+            } else if (num % 16 == 14) {
+                res.add("E");
+                num = Math.round(num / 16);
+            } else if (num % 16 == 15) {
+                res.add("F");
+                num = Math.round(num / 16);
+            } else {
+                res.add(Integer.toString(num % 16));
+                num = Math.round(num / 16);
+            }
+        }
+
+        return toString(res);
+    }
+
     public static void main(String[] args) {
         System.out.println(toBinary(87));
         System.out.println(toBinary(20));
@@ -41,5 +76,10 @@ public class NumConverter {
         System.out.println(toBinary(0));
         System.out.println(toBinary(4532));
         System.out.println(toBinary(97644));
+        System.out.println("---------------------------------");
+        System.out.println(toHex(87));
+        System.out.println(toHex(20));
+        System.out.println(toHex(347));
+        System.out.println(toHex(2220));
     }
 }
